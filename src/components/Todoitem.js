8577c49eb9290fccf81a9f4310cbd219c6cf8232
Ludmilla Mamelina <ludmilla@Ludmillas-MBP.fritz.box>
2020-02-17 17:31:00 +0100

    app completed

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -9,19 +9,33 @@ export class Todoitem extends Component{
             borderBottom: '1px #ccc dotted',
             textDecoration: this.props.todo.completed ? 'line-through':'none'}
     }
+
     render(){
+        const {id, title}=this.props.todo;
         return(
             <div style={this.getStyle()}>
-                <input type='checkbox' onChange={this.props.markComplete} />
-              <p>{this.props.todo.title}</p>  
+              <p><input type='checkbox' onChange={this.props.markComplete.bind(this, id)} />{' '}
+               {title}
+               <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+               </p>  
             </div>
         )
     }
 }
 
 Todoitem.propTypes={
-    todos: PropTypes.object.isRequired
+    todo: PropTypes.object.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
   }
 
-
+const btnStyle={
+    background: '#ff0000',
+    color: '#fff',
+    border:'none',
+    padding:'5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
 export default Todoitem
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,18 +3,18 @@ import Todoitem from './Todoitem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component{
-  markComplete=()=>{
-
-  }
+ 
   render(){
     console.log(this.props.todos)
     return this.props.todos.map((todo)=>(
-      <Todoitem key={todo.id} todo={todo} markComplete={this.markComplete}/>
+      <Todoitem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo}/>
     ))
   }
 }
 
 Todos.propTypes={
-  todos: PropTypes.array.isRequired
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 }
 export default Todos;
